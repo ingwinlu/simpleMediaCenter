0b0ad9e51248e3925b9991ce849b3f6236015e7d
winlu <derwinlu+git@gmail.com>
2014-03-28 15:52:46 +0100

    start to work on pathing

--- simpleMediaCenter/simpleMediaCenter.py
+++ simpleMediaCenter/simpleMediaCenter.py
@@ -2,6 +2,9 @@ from interface.WebInterface import WebInterface
 from player.Omxplayer import Omxplayer
 import logging
 
+pathToTemplates='./interface/templates/'
+pathToStatic='./interface/static'
+
 logging.basicConfig(level=logging.DEBUG)
 omxplayer = Omxplayer("")
 interface = WebInterface(player=omxplayer)
