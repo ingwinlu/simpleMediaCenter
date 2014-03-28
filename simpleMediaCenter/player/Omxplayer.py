c7132664127d3ebeed3433146e117b11bcc7eb4a
winlu <derwinlu+git@gmail.com>
2014-03-28 09:29:01 +0100

    inplemented omxplayer stop

--- simpleMediaCenter/player/Omxplayer.py
+++ simpleMediaCenter/player/Omxplayer.py
@@ -8,8 +8,6 @@ class Omxplayer(Player):
     __cmdline=""
     __playerline="omxplayer"
     __process=None
-    __stdin=None
-    __stdout=None
 
     def __init__(self, cmdline):
         self.cmdline=cmdline
@@ -23,18 +21,22 @@ class Omxplayer(Player):
             self.stop()
         
         line = self.__playerline + " " + self.__cmdline + " " + file
-        self.__process = subprocess.Popen(shlex.split(line), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        self.__process = subprocess.Popen(shlex.split(line), stdout=subprocess.PIPE, stdin=subprocess.PIPE , close_fds=True)
         
     def stop(self):
         logging.debug("stopping")
         if(self.__process is None):
             logging.debug("nothing to stop")
             return
-        self.__process.terminate()
+        self.__process.stdin.write(bytes('q', 'UTF-8'))
+        self.__process.stdin.flush()
         try:
+            logging.debug("waiting for process to close")
             self.__process.wait(timeout=5)
         except TimeoutExpired:
-            self.__process.kill()
+            logging.debug("timeout occured, killing")
+            #self.__process.kill()
+            subprocess.Popen(shlex.split("killall omxplayer.bin")).wait() ##quickhack
         self.__process = None    
         
 
@@ -44,9 +46,13 @@ if __name__ == "__main__":
     omxplayer = Omxplayer("");
     omxplayer.stop()
     omxplayer.play("test.mp4")
+    logging.debug("sleeping for 10 seconds")
     time.sleep(10)
+    logging.debug("done sleeping")
     omxplayer.play("test.mp4")
+    logging.debug("sleeping for 10 seconds")
     time.sleep(10)
+    logging.debug("done sleeping")
     omxplayer.stop()
     omxplayer.stop()
     
