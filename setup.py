134fbbfbac53e0c88d01777f59cbef983f82960f
winlu <derwinlu+git@gmail.com>
2014-03-29 10:56:19 +0100

    updated setup.py to include templates and css files

--- setup.py
+++ setup.py
@@ -8,6 +8,14 @@ setup(
 			'simpleMediaCenter.interface',
 			'simpleMediaCenter.player',
 			'simpleMediaCenter.playlist'],
+    package_data={
+            'simpleMediaCenter.interface' : ['templates/*.html','static/*.css'],
+            '' : ['LICENSE','README.md']            
+    },
+    requires = [
+        'jinja2',
+        'turbogears2'
+    ],
 	license=open('LICENSE').read(),
 	long_description=open('README.md').read(),
 	author='winlu',
