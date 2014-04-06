641a96e1126be75d0722413c58f69d0351f8a573
winlu <derwinlu+git@gmail.com>
2014-04-06 15:14:24 +0200

    added InterfaceListable

--- simpleMediaCenter/interface/Interface.py
+++ simpleMediaCenter/interface/Interface.py
@@ -8,3 +8,25 @@ class Displayable():
     def getDict(self):
         raise NotImplementedError
     
+class InterfaceListable():
+    __array=None
+    __current=0
+
+    def __init__(self, array):
+        if(array is None):
+            raise TypeError('array is not allowed to be None')
+            
+        if(len(array)<=0):
+            raise TypeError('array is not allowed to be of size 0 or smaller')
+            
+        self.__array=array
+        self.__current=0
+        
+    def setActive(self, id):
+        if(0<=id<len(self.__array)):
+            self.__current=id
+        else:
+            raise TypeError('id not in range of playerArray with size ' + len(self.__array))
+            
+    def getActive(self):
+        return self.__array[self.__current]
---
+++ test/TestInterfaceListable.py
@@ -0,0 +1,45 @@
+from interface.Interface import InterfaceListable
+import unittest
+import time
+import requests
+
+class TestInterfaceListable(unittest.TestCase):
+    interfaceListable = None
+
+    def setUp(self):
+        self.interfaceListable=InterfaceListable(['a','b','c'])
+        
+    def tearDown(self):
+        interfaceListable = None
+        
+        
+    def test_creation_1(self):
+        with self.assertRaises(TypeError):
+            InterfaceListable(None)
+        
+    def test_creation_2(self):
+        with self.assertRaises(TypeError):
+            InterfaceListable([])
+            
+    def test_set_outofbounds_1(self):
+        with self.assertRaises(TypeError):
+            self.interfaceListable.setActive(-1)
+            
+    def test_set_outofbounds_2(self):
+        with self.assertRaises(TypeError):
+            self.interfaceListable.setActive(3)
+            
+    def test_getActive(self):
+        interfaceListable=InterfaceListable(['a','b','c'])
+        self.assertEqual(interfaceListable.getActive(), 'a')
+        
+    def test_setActive(self):
+        interfaceListable=InterfaceListable(['a','b','c'])
+        interfaceListable.setActive(1)
+        self.assertEqual(interfaceListable.getActive(), 'b')
+        
+    def suite(self):
+        testSuite = unittest.TestSuite()
+        testSuite.addTest(unittest.makeSuite(TestInterfaceListable))
+        return testSuite
+        
