aa24baea028f5686b4c25be26fe7cdca78c302ec
winlu <derwinlu+git@gmail.com>
2014-04-13 23:45:18 +0200

    initial youtubeplayer commit

---
+++ simpleMediaCenter/player/Youtubeplayer.py
@@ -0,0 +1,59 @@
+from player.Omxplayer import Omxplayer
+from helpers.twitch import TwitchVideoResolver
+import logging
+import sys
+import os
+import subprocess
+import subprocess
+import shlex
+import time
+
+class Youtubeplayer(Omxplayer):
+    __youtubeprocess=None
+    __playerline="omxplayer --live"
+    __cmdline="-o both"
+    __tempfile=""
+    __youtubedlline=""
+    
+
+    def __init__(self, cmdline = "-o both"):
+        logging.debug("Youtubeplayer init")
+        self.__cmdline=cmdline
+        self.__tempfile="temp.mp4"
+        self.__youtubedlline="youtube-dl --no-part -o " + self.__tempfile
+        
+    def startDownload(self, url):
+        cmdline = self.__youtubedlline + " " + url
+        self.__youtubeprocess = subprocess.Popen(
+            shlex.split(cmdline), 
+            stdout=subprocess.PIPE, 
+            stdin=subprocess.PIPE, 
+            stderr=subprocess.PIPE, 
+            close_fds=True)
+        while (os.path.isfile(self.__tempfile) is False):
+            logging.debug("waiting for filecreation")
+            time.sleep(1)
+        return self.__tempfile
+        
+        
+    
+    def getcmdline(self,url):
+        self.startDownload(url)        
+        cmdline = self.__playerline + " " + self.__cmdline + " '" + self.__tempfile + "'"
+        logging.debug('Youtubeplayer cmdline: ' + cmdline)
+        return cmdline
+        
+    def stop(self):
+        super(Youtubeplayer, self).stop()
+        if (self.__youtubeprocess is not None):
+            try:
+                self.__youtubeprocess.terminate()
+                logging.debug("waiting for __youtubeprocess to close")
+                self.__youtubeprocess.wait(timeout=5)
+            except subprocess.TimeoutExpired:
+                self.__youtubeprocess.kill()
+            except ProcessLookupError:
+                pass
+        if (os.path.isfile(self.__tempfile)):
+            os.remove(self.__tempfile)
+            
---
+++ test/TestYoutubeplayer.py
@@ -0,0 +1,38 @@
+from player.Youtubeplayer import Youtubeplayer
+import unittest
+import time
+import requests
+import os
+
+class TestYoutubeplayer(unittest.TestCase):
+    youtubeplayer = None  
+    youtubeurl= "https://www.youtube.com/watch?v=Cwr-FhjE8Fw"
+
+    def setUp(self):
+        self.youtubeplayer = Youtubeplayer()
+        
+    def tearDown(self):
+        self.youtubeplayer.stop()
+        self.youtubeplayer=None
+        
+    def test_download(self):
+        filename = self.youtubeplayer.startDownload(self.youtubeurl)
+        self.assertTrue(os.path.isfile(filename))
+        
+    def test_playback(self):
+        self.assertEqual(self.youtubeplayer.getDict()['playerStatus'], 0)
+        self.youtubeplayer.play(self.youtubeurl)
+        time.sleep(10)
+        self.youtubeplayer.pause()   
+        self.assertEqual(self.youtubeplayer.getDict()['playerStatus'], 2)
+        self.youtubeplayer.pause()  
+        self.assertEqual(self.youtubeplayer.getDict()['playerStatus'], 1)
+        self.youtubeplayer.stop()  
+        self.assertEqual(self.youtubeplayer.getDict()['playerStatus'], 0)
+
+        
+    def suite(self):
+        testSuite = unittest.TestSuite()
+        testSuite.addTest(unittest.makeSuite(TestYoutubeplayer))
+        return testSuite
+        
