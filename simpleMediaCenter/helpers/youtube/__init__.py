1cd2fc5c72a1ffae0b5aa5215b8bf6d6e78e7566
winlu <derwinlu+git@gmail.com>
2014-04-15 16:51:40 +0200

    startet work on youtube helper

---
+++ simpleMediaCenter/helpers/youtube/__init__.py
@@ -0,0 +1,62 @@
+#-*- encoding: utf-8 -*-
+try:
+    from urllib.request import urlopen, Request
+    from urllib.parse import quote_plus
+except ImportError:
+    from urllib import quote_plus
+    from urllib2 import Request, urlopen
+
+import logging
+import sys
+import xml.etree.ElementTree as et
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0'
+
+class XMLScraper():
+    
+    def downloadWebData(self, url):
+        data = ""
+        try:
+            req = Request(url)
+            req.add_header('User-Agent', USER_AGENT)
+            response = urlopen(req)
+            
+            if sys.version_info < (3, 0):
+                data = response.read()
+            else:
+                data = response.readall().decode('utf-8')
+            response.close()
+        except:
+            raise Exception()
+        return data
+    
+    def downloadXML(self, url):
+        try:
+            xmlString = self.downloadWebData(url)
+            tree = et.fromstring(xmlString)
+            logging.debug(et.dump(tree))
+            return tree
+        except:
+            raise Exception()
+    
+    
+class Youtube():
+    NAMESPACES = {
+        'Atom': 'http://www.w3.org/2005/Atom',
+        'openSearch': 'http://a9.com/-/spec/opensearchrss/1.0/'}
+    URLS = {
+        'uploads':'https://gdata.youtube.com/feeds/api/users/{0[0]}/uploads?start-index={0[1]}&max-results={0[2]}'}
+    
+    __scraper = None
+    
+    
+    def __init__(self):
+        self.__scraper=XMLScraper()
+        
+    def __prepareURL(self, url_key, replacement_list):
+        return self.URLS[url_key].format(replacement_list)
+        
+    def listChannelVideos(self, channelName, startindex=1, maxresults=25):
+        url = self.__prepareURL('uploads', (channelName, startindex, maxresults))
+        tree = self.__scraper.downloadXML(url)
+        
--- simpleMediaCenter/player/Youtubeplayer.py
+++ simpleMediaCenter/player/Youtubeplayer.py
@@ -3,10 +3,6 @@ import youtube_dl
 import logging
 import sys
 import os
-import subprocess
-import shlex
-import time
-import threading
 
 class Youtubeplayer(Omxplayer):
     __playerline="omxplayer -b --live"
@@ -16,11 +12,12 @@ class Youtubeplayer(Omxplayer):
     def __init__(self, cmdline = "-o both"):
         logging.debug("Youtubeplayer init")
         self.__cmdline=cmdline
-        self.__ytdl=youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+        self.__ytdl=youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
         self.__ytdl.add_default_info_extractors()
   
     def getcmdline(self,url):
         result = self.__ytdl.extract_info(url,download=False)
+        #insert resultcheck here
         cmdline = self.__playerline + " " + self.__cmdline + " '" + result['url'] + "'"
         logging.debug('Youtubeplayer cmdline: ' + cmdline)
         return cmdline
